cbcdce1373f37149fb4cb40ebb7724876897d127
Ji-hoon Lee <dalton.lee@samsung.com>
2014-02-28 19:12:52 +0900

    Added missing EFL animator backend
    Change-Id: I281b5f3d7f39a1c3cca8532b326e553c651aefbf

---
+++ scl/gwes/efl/sclanimator-efl.cpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2012-2013 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Flora License, Version 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://floralicense.org/license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "sclanimator-efl.h"
+#include "scldebug.h"
+
+#include <Ecore.h>
+#include <Elementary.h>
+
+#include "sclutils.h"
+#include "sclwindows.h"
+
+using namespace scl;
+
+/**
+ * Constructor
+ */
+CSCLAnimatorImplEfl::CSCLAnimatorImplEfl()
+{
+    SCL_DEBUG();
+
+    m_highlight_ui_object = NULL;
+}
+
+/**
+ * De-constructor
+ */
+CSCLAnimatorImplEfl::~CSCLAnimatorImplEfl()
+{
+    SCL_DEBUG();
+}
+
+void CSCLAnimatorImplEfl::init()
+{
+
+}
+
+void CSCLAnimatorImplEfl::fini()
+{
+    if (m_highlight_ui_object) {
+        evas_object_del(m_highlight_ui_object);
+        m_highlight_ui_object = NULL;
+    }
+}
+
+
+sclboolean
+CSCLAnimatorImplEfl::check_animation_supported()
+{
+    /* EFL Backend finished implementing animation support */
+    return TRUE;
+}
+
+void
+CSCLAnimatorImplEfl::animator_timer(SclAnimationState *state)
+{
+    CSCLUtils *utils = CSCLUtils::get_instance();
+    CSCLWindows *windows = CSCLWindows::get_instance();
+
+    if (state && windows && utils) {
+        switch (state->desc.type) {
+            case ANIMATION_TYPE_HIGHLIGHT_UI: {
+                if (state->active) {
+                    SclRectangle rect = state->rect_cur;
+                    if (m_highlight_ui_object == NULL) {
+                        Evas_Object *window_object = static_cast<Evas_Object*>(windows->get_base_window());
+                        Evas *evas = evas_object_evas_get(window_object);
+                        m_highlight_ui_object = evas_object_image_add(evas);
+                        sclchar composed_path[_POSIX_PATH_MAX] = {0,};
+                        utils->get_composed_path(composed_path, IMG_PATH_PREFIX, SCL_HIGHLIGHT_UI_IMAGE);
+                        evas_object_image_file_set(m_highlight_ui_object, composed_path, NULL);
+                    }
+                    evas_object_move(m_highlight_ui_object, rect.x, rect.y);
+                    evas_object_image_fill_set(m_highlight_ui_object, 0, 0, rect.width, rect.height);
+                    evas_object_resize(m_highlight_ui_object, rect.width, rect.height);
+                    evas_object_raise(m_highlight_ui_object);
+                    evas_object_show(m_highlight_ui_object);
+
+                    sclint window_layer = 29000;
+                    if (!windows->is_base_window(state->desc.window_from) ||
+                        !windows->is_base_window(state->desc.window_to)) {
+                            window_layer = 29010;
+                    }
+                    evas_object_layer_set(m_highlight_ui_object, window_layer + 1);
+                } else {
+                    evas_object_hide(m_highlight_ui_object);
+                }
+            }
+            break;
+        }
+    }
+}
---
+++ scl/gwes/efl/sclanimator-efl.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2012-2013 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Flora License, Version 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://floralicense.org/license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "sclanimator.h"
+
+#ifndef __SCL_ANIMATOR_EFL_H__
+#define __SCL_ANIMATOR_EFL_H__
+
+#include <Evas.h>
+
+namespace scl
+{
+class CSCLAnimatorImplEfl : public CSCLAnimatorImpl
+{
+public :
+    CSCLAnimatorImplEfl();
+    ~CSCLAnimatorImplEfl();
+
+    void init();
+    void fini();
+
+    sclboolean check_animation_supported();
+    void animator_timer(SclAnimationState *state);
+
+private:
+    Evas_Object *m_highlight_ui_object;
+};
+} /* End of scl namespace */
+#endif //__SCL_ANIMATOR_EFL_H__
